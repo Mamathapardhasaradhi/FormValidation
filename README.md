477425804bfe1eca564799f43bd5382ce1117ef1
Mamatha <mamatha>
2022-08-25 16:08:26 +0530

    Added two validations.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # FormValidation
 
-A description of this package.
+This package can be used for Textfield input validations. It returns a bool value after validating the input text. This makes developers easy for validating all the textfield input values.
--- Sources/FormValidation/FormValidation.swift
+++ Sources/FormValidation/FormValidation.swift
@@ -3,4 +3,40 @@ public struct FormValidation {
 
     public init() {
     }
+    
+    public func isValidEmail(emailText: String) -> Bool {
+        if(emailText.count > 0) {
+            var isEmail: Bool {
+                do {
+                    let regex = try NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", options: .caseInsensitive)
+                    return regex.firstMatch(in: emailText, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, emailText.count)) != nil
+                } catch {
+                    return false
+                }
+            }
+            if(isEmail == true) {
+                return true
+            } else {
+                return false
+            }
+        }
+        return false
+    }
+    
+    public func isValidMobileNumber(mobileNumberText: String) -> Bool {
+        if(mobileNumberText.count > 0) {
+            var isPhoneNumber: Bool {
+                let PHONE_REGEX = "^[0-9]{8,13}$"//^[789]\\d{9}$
+                let phoneTest = NSPredicate(format: "SELF MATCHES %@", PHONE_REGEX)
+                let result =  phoneTest.evaluate(with: mobileNumberText)
+                return result
+            }
+            if(isPhoneNumber == true) {
+                return true
+            } else {
+                return false
+            }
+        }
+        return false
+    }
 }
